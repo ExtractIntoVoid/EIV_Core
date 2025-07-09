062ca7c6b0d1c2033cb0fe9fed66a54254b6e657
SlejmUr <slejmur@protonmail.com>
2025-07-09 15:32:58 +0200

    fix mext

--- EIV_Modules/Extensions/ModuleExtensions.cs
+++ EIV_Modules/Extensions/ModuleExtensions.cs
@@ -6,28 +6,28 @@ public static class ModuleExtensions
 {
     public static T GetModule<T>(this object obj) where T : IModule
     {
-        return (T?)ModuleSub?.Invoke(obj, typeof(T));
+        return (T?)ModuleSub.GetFirstModule?.Invoke(obj, typeof(T));
     }
 
-    public static bool TryGetModuleNode<T>(this object obj, out T out_t) where T : IModule
+    public static List<T> GetModules<T>(this object obj) where T : IModule
+    {
+        return (List<T>)ModuleSub.GetModuleList?.Invoke(obj, typeof(T));
+    }
+
+    public static bool TryGetModule<T>(this object obj, out T out_t) where T : IModule
     {
         out_t = default;
-        out_t = (T)node.GetChildren().FirstOrDefault(x => x is T);
+        out_t = (T?)ModuleSub.GetFirstModule?.Invoke(obj, typeof(T));
         return out_t != null;
     }
 
-    public static void AddModuleNode<T>(this object obj, T module) where T : IModule
+    public static void AddModule<T>(this object obj, T module) where T : IModule
     {
-        module.SetMultiplayerAuthority(node.GetMultiplayerAuthority());
-        node.AddChild(module);
+        ModuleSub.AddModule?.Invoke(obj, module);
     }
 
-    public static void RemoveModuleNode<T>(this object obj) where T : IModule
+    public static void RemoveModule<T>(this object obj) where T : IModule
     {
-        var NodeList = node.GetChildren().Where(x=>x is T).ToList();
-        foreach(var Node in NodeList)
-        {
-            node.RemoveChild(Node);
-        }
+        ModuleSub.RemoveModule?.Invoke(obj, typeof(T));
     }
 }
